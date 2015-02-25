83b7dd15607c3e03288f96b814f195fd1e6fe164
Alessandro Dalfovo <alessandro.dalfovo@paros.tn.it>
2015-02-25 14:35:33 +0100

    Solving problems 3 4 5

--- lists.clj
+++ lists.clj
@@ -10,10 +10,45 @@
 
 (defn penultimate [x]
   "P02 (*) Find the penultimate element of a list"
-    (loop [acc 0 list x]
-      (if (next list)
-        (recur (first list) (rest list))
-        acc
-      )
+  (loop [acc 0 list x]
+    (if (next list)
+      (recur (first list) (rest list))
+      acc
     )
+  )
+)
+
+(defn element-at [list idx]
+  "P03 (*) Find the K'th element of a list"
+  (loop [count 1 xs list]
+    (if (= count idx)
+      (first xs)
+      (recur (+ count 1)(next xs))
+    )
+  )
+)
+
+(defn size-of[list]
+  "P04 (*) Find the number of elements of a list"
+  (if (nil? list) 0)
+  (loop [count 1 xs list]
+    (if (next xs)
+      (recur (+ count 1)(next xs))
+      count
+    )
+  )
+)
+
+
+(defn reverse-of[list]
+  "P05 (*) Reverse a list"
+  (loop [rev () xs list]
+    (if (first xs)
+      (recur
+        (conj rev (first xs))
+        (next xs)
+      )
+      rev
+    )
+  )
 )
---
+++ readme.md
@@ -0,0 +1,3 @@
+I'm Trying to solve the 99 Lisp Problems in Clojure!
+
+http://www.ic.unicamp.br/~meidanis/courses/mc336/2006s2/funcional/L-99_Ninety-Nine_Lisp_Problems.html
